31ee2088f01b11bf7d3223be31cea5088b24a793
Tomoya-Mukai514 <mukai.t@hotmail.co.jp>
2026-04-09 07:25:08 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
+  if (req.method === "GET" && req.url === "/health") {
+    return next();
+  }
+
   console.log(new Date().toISOString(), req.method, req.url);
   next();
 });
